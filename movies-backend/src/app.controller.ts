2065add122e21df52fc85ce0aee68ffc6037feca
Leul Abay <leulabay1@gmail.com>
2023-07-30 11:54:21 +0300

    minor changes

--- movies-backend/src/app.controller.ts
+++ movies-backend/src/app.controller.ts
@@ -2,9 +2,12 @@ import { Controller, Get,Post,Req, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from 'express';
 
-@Controller('/user')
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Get()
+  getuser(): string {
+    return "hello world";
+  }
  
 }
